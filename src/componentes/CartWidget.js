82d30c6a32b8cf67dc969350dd1545cece6f4191
Alvaro Mora <alyua76@gmail.com>
2023-04-05 21:42:56 -0400

    ItemListContainer Agregado

--- src/componentes/CartWidget.js
+++ src/componentes/CartWidget.js
@@ -3,7 +3,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 const CartWidget = () => {
   return (
     <div>
-      <img src={ShoppingCartIcon} alt="Cart-Widget"></img>0
+      <ShoppingCartIcon />0
     </div>
   );
 };
