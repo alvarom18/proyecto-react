ae4b1dbe5e4942298279396241586f828e616bd4
Alvaro Mora <alyua76@gmail.com>
2023-05-20 00:00:02 -0400

    Proyecto

---
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import ItemCount from "../Counter/Counter";
+
+const ItemDetail = ({id, name, img, category, description, price, stock}) => {
+    const[quantityAdded, set]
+}
